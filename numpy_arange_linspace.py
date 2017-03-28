c9149c99d09be88fa549b54e8574174ae5761553
wigging <wigging@me.com>
2017-03-28 00:58:23 -0400

    organize files

--- numpy_arange_linspace.py
+++ numpy_arange_linspace.py
@@ -1,16 +1,8 @@
+"""
+Examples of arange and linspace in NumPy.
+"""
+
 import numpy as np
-
-# range examples using Python
-
-r1 = range(10)
-r2 = range(1, 10)
-r3 = range(-2, 8)
-r4 = range(0, 10, 2)
-
-print('r1 is', list(r1))
-print('r2 is', list(r2))
-print('r3 is', list(r3))
-print('r4 is', list(r4))
 
 # arange examples using NumPy
 
---
+++ python_range.py
@@ -0,0 +1,14 @@
+"""
+Examples of range in Python. See numpy_arange_linspace.py for other examples.
+"""
+
+r1 = range(10)
+r2 = range(1, 10)
+r3 = range(-2, 8)
+r4 = range(0, 10, 2)
+
+print('r1 is', list(r1))
+print('r2 is', list(r2))
+print('r3 is', list(r3))
+print('r4 is', list(r4))
+
