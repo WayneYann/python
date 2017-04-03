08431bb51b68075d31a269b7629db673d21add4d
wigging <wigging@me.com>
2017-04-02 21:27:35 -0400

    use enumerate for index and item from list

---
+++ python_enumerate.py
@@ -0,0 +1,10 @@
+"""
+Use enumerate to get items and each index from a list.
+"""
+
+my_items = ['a', 'b', 'c', 'd', 'e']
+
+for idx, item in enumerate(my_items):
+    print('idx =', idx, 'and item =', item)
+
+
