e12de82b1877c473fd9f13652c5ae1f7c2084191
wigging <wigging@me.com>
2017-10-09 20:26:58 -0400

    update bar chart example

--- matplotlib_bar.py
+++ matplotlib_bar.py
@@ -2,19 +2,29 @@
 Simple bar chart using Matplotlib
 """
 
-import matplotlib.pyplot as py
+import matplotlib.pyplot as plt
 import numpy as np
 
-# parameters
-items = np.array([1, 2, 3, 4, 5])
+# Prepare for plotting
 
+plt.ion()
+plt.close('all')
+
+# Example 1
+
+items = np.array([1, 2, 3, 4, 5])
 data = np.array([10, 8, 5, 7, 9])
 data2 = np.array([8, 9, 6, 8, 7])
 
-# bar plot
-py.ion()
-py.close('all')
-py.figure(1)
-py.bar(items, data, color='g', width=0.4, align='center', alpha=0.5)
-py.bar(items + 0.4, data2, color='b', width=0.4, align='center', alpha=0.5)
-py.xticks(items + 0.2, ['aaa', 'bbb', 'ccc', 'ddd', 'eee'])
+plt.figure(1)
+plt.bar(items, data, color='g', width=0.4, align='center', alpha=0.5)
+plt.bar(items + 0.4, data2, color='b', width=0.4, align='center', alpha=0.5)
+plt.xticks(items + 0.2, ['aaa', 'bbb', 'ccc', 'ddd', 'eee'])
+
+# Example 2
+
+data3 = {'apples': 10, 'oranges': 15, 'lemons': 5, 'limes': 20}
+
+plt.figure(2)
+plt.bar(data3.keys(), data3.values())
+
